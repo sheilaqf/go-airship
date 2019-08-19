f467852e4cb4f8c331e140e019b1a9ec235ac1f9
Sebastian Döll <sebastian@katallaxie.me>
2019-08-19 13:14:40 +0200

    feat: adding custom events reports

--- reports.go
+++ reports.go
@@ -29,6 +29,48 @@ type ReportsDevicesParams struct {
 	Date string `url:"date,omitempty"`
 }
 
+// ReportsEventsResponse ...
+type ReportsEventsResponse struct {
+	// OK ...
+	OK bool `json:",omitempty"`
+	// TotalValue ...
+	TotalValue int `json:"total_value,omitempty"`
+	// TotalCount ...
+	TotalCount int `json:"total_count,omitempty"`
+	// NextPage ...
+	NextPage string `json:"next_page,omitempty"`
+	// Events ...
+	Events []*ReportsEvent `json:"events,omitempty"`
+}
+
+// ReportsEvent ...
+type ReportsEvent struct {
+	// Name ...
+	Name string `json:"name,omitempty"`
+	// Conversion ...
+	Conversion string `json:"conversion,omitempty"`
+	// Location ...
+	Location string `json:"location,omitempty"`
+	// Count ...
+	Count int `json:"count,omitempty"`
+	// Count ...
+	Value int `json:"value,omitempty"`
+}
+
+// ReportsEventsParams ...
+type ReportsEventsParams struct {
+	// Start ...
+	Start string `url:"start"`
+	// End ...
+	End string `url:"end"`
+	// Precision ...
+	Precision string `url:"precision"`
+	// Page ...
+	Page int `url:"page,omitempty"`
+	// PageSize ...
+	PageSize int `url:"page_size,omitempty"`
+}
+
 func newReportsService(sling *sling.Sling) *ReportsService {
 	return &ReportsService{
 		sling: sling,
@@ -41,6 +83,24 @@ func (r *ReportsService) Devices(params *ReportsDevicesParams) (*ReportsDevicesR
 	failure := new(AirshipError)
 
 	res, err := r.sling.New().Get(path.Join(ReportsPath, ReportsDevicesPath)).QueryStruct(params).Receive(success, failure)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != 200 {
+		return nil, failure
+	}
+
+	return success, nil
+}
+
+// CustomEvents ...
+func (r *ReportsService) Events(params *ReportsEventsParams) (*ReportsEventsResponse, error) {
+	success := new(ReportsEventsResponse)
+	failure := new(AirshipError)
+
+	res, err := r.sling.New().Get(path.Join(ReportsPath, ReportsEventsPath)).QueryStruct(params).Receive(success, failure)
 
 	if err != nil {
 		return nil, err
--- reports_test.go
+++ reports_test.go
@@ -10,30 +10,15 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("App", func() {
+var _ = Describe("Reports", func() {
 
 	var client *airship.Client
 	var httpClient *http.Client
 	var mux *http.ServeMux
 	var server *httptest.Server
-	var resp *airship.ReportsDevicesResponse
 
 	BeforeEach(func() {
 		httpClient, mux, server = fakeTestServer()
-
-		resp = &airship.ReportsDevicesResponse{
-			TotalUniqueDevices: 1,
-			DateClosed:         "2019-08-18 00:00:00",
-			DateComputed:       "2019-08-19 06:08:04",
-			Counts: map[string]map[string]int{
-				"ios": map[string]int{
-					"unique_devices": 1,
-					"opted_in":       1,
-					"opted_out":      1,
-					"uninstalled":    1,
-				},
-			},
-		}
 
 		client = airship.New(httpClient, airship.EndpointURL(airship.AirshipNorthAmericaURL))
 	})
@@ -42,12 +27,27 @@ var _ = Describe("App", func() {
 		defer server.Close()
 	})
 
-	Describe("Get Device Reports with empty query", func() {
+	Describe("Get devices reports with empty query", func() {
 		var res *airship.ReportsDevicesResponse
 		var params *airship.ReportsDevicesParams
+		var resp *airship.ReportsDevicesResponse
 		var err error
 
 		BeforeEach(func() {
+			resp = &airship.ReportsDevicesResponse{
+				TotalUniqueDevices: 1,
+				DateClosed:         "2019-08-18 00:00:00",
+				DateComputed:       "2019-08-19 06:08:04",
+				Counts: map[string]map[string]int{
+					"ios": map[string]int{
+						"unique_devices": 1,
+						"opted_in":       1,
+						"opted_out":      1,
+						"uninstalled":    1,
+					},
+				},
+			}
+
 			mux.HandleFunc("/api/reports/devices", func(w http.ResponseWriter, r *http.Request) {
 				Ω(r.Method).Should(Equal("GET"))
 
@@ -107,6 +107,62 @@ var _ = Describe("App", func() {
 
 		It("should contain all the information", func() {
 			Ω(err).Should(Equal(airshipErr))
+		})
+	})
+
+	Describe("Get events report with only required params", func() {
+		var res *airship.ReportsEventsResponse
+		var params *airship.ReportsEventsParams
+		var resp *airship.ReportsEventsResponse
+		var err error
+
+		BeforeEach(func() {
+			resp = &airship.ReportsEventsResponse{
+				OK:         true,
+				TotalValue: 1,
+				TotalCount: 1,
+				NextPage:   "https://go.urbanairship.com/api/reports/events?start=2018-08-01T10:00:00.000Z&end=2018-08-15T20:00:00.000Z&precision=MONTHLY&page_size=20&page=2",
+				Events: []*airship.ReportsEvent{
+					&airship.ReportsEvent{
+						Name:       "banner_image",
+						Conversion: "indirect",
+						Count:      1,
+						Value:      1,
+					},
+				},
+			}
+
+			mux.HandleFunc("/api/reports/events", func(w http.ResponseWriter, r *http.Request) {
+				Ω(r.Method).Should(Equal("GET"))
+
+				p := r.URL.Query()
+				Ω(p.Get("start")).Should(Equal(params.Start))
+				Ω(p.Get("end")).Should(Equal(params.End))
+				Ω(p.Get("precision")).Should(Equal(params.Precision))
+
+				w.Header().Set("Content-Type", "application/vnd.urbanairship+json; version=3;")
+
+				b, err := json.Marshal(resp)
+				Ω(err).Should(BeNil())
+
+				w.Write(b)
+			})
+
+			params = &airship.ReportsEventsParams{
+				Start:     "2018-08-28 00:00:00",
+				End:       "2018-08-29 13:30:45",
+				Precision: "HOURLY",
+			}
+
+			res, err = client.Reports.Events(params)
+		})
+
+		It("should not return an error", func() {
+			Ω(err).Should(BeNil())
+		})
+
+		It("should contain all the information", func() {
+			Ω(res).Should(Equal(resp))
 		})
 	})
 })
